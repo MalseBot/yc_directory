2575f42373e2f767b4a450f8001954ffdae55086
Eslam Hassanin <110671544+MalseBot@users.noreply.github.com>
2024-12-08 02:52:40 +0200

    auth code

---
+++ app/components/navbar.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import {auth, signIn, signOut} from "@/auth";
+
+
+const Navbar =async () => {
+    const session = await auth();
+    return (
+        <header className="px-5 py-3 font-work-sans shadow-sm text-black bg-white">
+            <nav className={'flex justify-between items-center'}>
+                <Link href={'/'}><Image src={'/logo.png'} alt={'logo'} width={144} height={30}/></Link>
+                <div className={'flex items-center gap-5'}>
+                    {session && session?.user ?(
+                        <>
+                            <Link href={'/startup/create'}><span>Create</span></Link>
+                            <form action={async () => {
+                                "use server"
+                                await signOut({redirectTo:"/"})
+                            }}>
+                                <button type={'submit'} className={''}>Logout</button>
+                            </form>
+                            <Link href={`/user.${session?.user?.id}`}><span>{session?.user?.name}</span></Link>
+                        </>
+                    ) : (<form
+                        action={async () => {
+                            "use server"
+                            await signIn("google")
+                        }}
+                    >
+                        <button type="submit">Signin with Google</button>
+                    </form>)
+
+                    }
+                </div>
+            </nav>
+        </header>
+    )
+}
+export default Navbar
